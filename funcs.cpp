543063ee210bc5ae4e27d54044b89e7729884882
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-04 05:55:01 -0400

    frequencies

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include "funcs.h"
 
-// add functions here
 
+int getIndex(char c) {
+  if (isupper(c)) {
+    return c - 65;
+  }
+  return c - 97;
+}
+
+int getLowerChar(int index) {
+  return 97 + index;
+}
+
+double getFrequency(std::string str, char letter) {
+  int count = 0;
+  for (auto c: str) {
+    if (c == tolower(letter)) {
+      count++;
+    } else if (c == toupper(letter)) {
+      count++;
+    }
+  }
+  return ((double) count) / str.size();
+}
+
+
+std::vector<double> getFrequencies(std::string str) {
+  std::vector<double> frequencies;
+  for (int i = 0; i < 26; i++) {
+    frequencies.push_back(getFrequency(str, getLowerChar(i)));
+  }
+  return frequencies;
+}
--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,32 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+TEST_CASE("relative index of letter") {
+  CHECK_EQ(getIndex('a'), 0);
+  CHECK_EQ(getIndex('A'), 0);
+  CHECK_EQ(getIndex('Z'), 25);
+  CHECK_EQ(getIndex('z'), 25);
+  CHECK_EQ(getIndex('l'), 11);
+  CHECK_EQ(getIndex('L'), 11);
+}
+
+TEST_CASE("individual frequency of letter") {
+  CHECK_EQ(getFrequency("helLo", 'l'), 0.4);
+  CHECK_EQ(getFrequency("hi", 'z'), 0);
+  CHECK_EQ(getFrequency("hello", 'l'), 0.4);
+}
+
+TEST_CASE("reverse index to lowercase letter") {
+  CHECK_EQ(getLowerChar(0), 'a');
+  CHECK_EQ(getLowerChar(25), 'z');
+  CHECK_EQ(getLowerChar(3), 'd');
+}
+
+TEST_CASE("letter frequencies") {
+  std::vector<double> test1 = {0, 0, 0, 0, 0.2, 0, 0, 0.2, 0, 0, 0, 0.4, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  CHECK_EQ(getFrequencies("hello"), test1);
+  CHECK_EQ(getFrequencies("HEllO"), test1);
+  CHECK_EQ(ENGLISH_FREQUENCY[9], 0.0015);
+}
