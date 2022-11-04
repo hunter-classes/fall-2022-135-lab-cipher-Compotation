ec865c01544756ffd9590cc15e49b1a6dc90234a
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-04 06:39:33 -0400

    update main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 int main()
 {
-  std::cout << getIndex('j');
+  std::string encrypted = "N++ td ly zmupne zctpyepo wlyrflrp lyo dzxp nzynpaed xlj mp yph. Elvp mcplvd hspy yppopo, lyo rz zgpc esp pilxawpd ld xlyj etxpd ld yppopo.";
+  std::string answer = "C++ is an object oriented language and some concepts may be new. Take breaks when needed, and go over the examples as many times as needed.";
+  std::cout << "encrypted: " << encrypted << "\n";
+  std::cout << "answer: " << answer << "\n";
+  std::cout << "decrypted: " << solve(encrypted) << std::endl;
   return 0;
 }
